315d5fda7bc26e7a242169ef349b1ae3bc7ce63b
unknown <edgarerb@hotmail.es>
2023-08-13 16:19:12 +0200

    test de funcion navegar aprovechando zonejs,no se usara zonejs al final,solo prueba para conocerlo mejor por codigos antiguos

---
+++ appComida/src/app/core/components/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component,inject } from '@angular/core';
+import { HeaderService } from '../../services/header.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+
+  headerService = inject(HeaderService)
+
+}
--- appComida/src/app/pages/home/home.component.ts
+++ appComida/src/app/pages/home/home.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { HeaderService } from 'src/app/core/services/header.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  headerService = inject(HeaderService)
+
+
+  ngOnInit(): void {
+    this.headerService.titulo = "home"
+  }
+
+
 
 }
