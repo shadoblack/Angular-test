6eadff0943c7d576faaefe94136e4b592e393325
unknown <edgarerb@hotmail.es>
2023-08-20 17:23:47 +0200

    algunos estilos modificados y pruebas con botones de carrito

--- appComida/src/app/pages/articulo/articulo.component.ts
+++ appComida/src/app/pages/articulo/articulo.component.ts
@@ -1,22 +1,24 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Producto } from 'src/app/core/interfaces/productos';
 import { HeaderService } from 'src/app/core/services/header.service';
 import { ProductosService } from 'src/app/core/services/productos.service';
+import { ContadorCantidadComponent } from "../../core/components/contador-cantidad/contador-cantidad.component";
 
 @Component({
-  selector: 'app-articulo',
-  templateUrl: './articulo.component.html',
-  styleUrls: ['./articulo.component.scss'],
-  standalone: true,
-  imports: [CommonModule]
+    selector: 'app-articulo',
+    templateUrl: './articulo.component.html',
+    styleUrls: ['./articulo.component.scss'],
+    standalone: true,
+    imports: [CommonModule, ContadorCantidadComponent]
 })
 export class ArticuloComponent {
   headerService = inject(HeaderService);
   productosService = inject(ProductosService);
 
   producto?: Producto;
+  cantidad = signal(1);
 
   ngOnInit(): void {
     this.headerService.titulo.set('Artículo');
