03fed1618db2a14f5697458a1491849fc872e4e8
unknown <edgarerb@hotmail.es>
2023-08-26 18:05:17 +0200

    apaños al carrito

--- appComida/src/app/pages/carrito/carrito.component.ts
+++ appComida/src/app/pages/carrito/carrito.component.ts
@@ -3,6 +3,8 @@ import { Component, inject } from '@angular/core';
 import { CartService } from 'src/app/core/services/cart.service';
 import { HeaderService } from 'src/app/core/services/header.service';
 import { ContadorCantidadComponent } from "../../core/components/contador-cantidad/contador-cantidad.component";
+import { Producto } from 'src/app/core/interfaces/productos';
+import { ProductosService } from 'src/app/core/services/productos.service';
 
 @Component({
     selector: 'app-carrito',
@@ -14,9 +16,17 @@ import { ContadorCantidadComponent } from "../../core/components/contador-cantid
 export class CarritoComponent {
   headerService = inject(HeaderService);
   cartService = inject(CartService);
+  productosService = inject(ProductosService)
+
+  productosCarrito:Producto[]=[];
 
   ngOnInit(): void {
     this.headerService.titulo.set('Carrito');
+    this.cartService.carrito.forEach( async itemCarrito =>{
+     const res = await this.productosService.getById(itemCarrito.idProducto)
+     if(res) this.productosCarrito.push(res);
+    })
+    
   }
 
   eliminarProducto(idProducto:number){
