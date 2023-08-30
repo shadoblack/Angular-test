658b3a6eb8241c3cd33e4bccb9e42f33f64b0f89
unknown <edgarerb@hotmail.es>
2023-08-30 17:59:58 +0200

    perfil hecho

--- appComida/src/app/pages/perfil/perfil.component.ts
+++ appComida/src/app/pages/perfil/perfil.component.ts
@@ -1,17 +1,38 @@
-import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, isStandalone } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Perfil } from 'src/app/core/interfaces/perfil';
 import { HeaderService } from 'src/app/core/services/header.service';
+import { PerfilService } from 'src/app/core/services/perfil.service';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
-  styleUrls: ['./perfil.component.scss']
+  styleUrls: ['./perfil.component.scss'],
+  standalone: true,
+  imports: [CommonModule, FormsModule]
 })
 export class PerfilComponent {
-  headerService = inject(HeaderService)
+  headerService = inject(HeaderService);
+  perfilService = inject(PerfilService)
 
 
   ngOnInit(): void {
-    this.headerService.titulo.set("Perfil")
+    this.headerService.titulo.set("Perfil");
+    if(this.perfilService.perfil()){
+      this.perfil = {...this.perfilService.perfil()!}
+    }
+  }
+
+  perfil:Perfil = {
+    nombre:"",
+    direccion:"",
+    telefono:"",
+    detalleEntrega: ""
+  }
+
+  guardarDatosPerfil(){
+    this.perfilService.guardarDatos(this.perfil);
   }
 
 }
