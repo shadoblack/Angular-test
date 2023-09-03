5c6658dab69d9e8ca8c5722e64d5785a6ee3dd09
unknown <edgarerb@hotmail.es>
2023-09-03 17:03:57 +0200

    busqueda operativa

--- appComida/src/app/core/services/productos.service.ts
+++ appComida/src/app/core/services/productos.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Producto } from '../interfaces/productos';
 import { Categoria } from '../interfaces/categorias';
+import { Busqueda } from '../interfaces/busqueda';
 
 @Injectable({
   providedIn: 'root',
@@ -18,19 +19,39 @@ export class ProductosService {
     return [];
   }
 
-  async getAll():Promise<Producto[]>{
+  async getAll(): Promise<Producto[]> {
     const res = await fetch('./../../../assets/data/database.json');
-    const resJson:Categoria[] = await res.json();
-    let productos:Producto[] = [];
-    resJson.forEach(categoria =>{
-      productos = [...productos, ...categoria.productos]
-    })
+    const resJson: Categoria[] = await res.json();
+    let productos: Producto[] = [];
+    resJson.forEach((categoria) => {
+      productos = [...productos, ...categoria.productos];
+    });
     return productos;
-}
+  }
 
-async getById(id:number):Promise<Producto | undefined>{
+  async getById(id: number): Promise<Producto | undefined> {
     const productos = await this.getAll();
-    const productoElegido = productos.find(producto => producto.id === id);
+    const productoElegido = productos.find((producto) => producto.id === id);
     return productoElegido ? productoElegido : undefined;
-}
+  }
+
+  async buscar(parametros: Busqueda) {
+    const productos = await this.getAll();
+    const productosFiltrados = productos.filter((producto) => {
+      if (parametros.aptoCeliaco && !producto.esCeliaco) return false;
+      if (parametros.aptoVegano && !producto.esVegano) return false;
+      const busquedaTitulo = producto.nombre
+        .toLowerCase()
+        .includes(parametros.texto.toLowerCase());
+      console.log(busquedaTitulo);
+      if (busquedaTitulo) return true;
+      for (let i = 0; i < producto.ingredientes.length; i++) {
+        const ingrediente = producto.ingredientes[i];
+        if (ingrediente.toLowerCase().includes(parametros.texto.toLowerCase()))
+          return true;
+      }
+      return false;
+    });
+    return productosFiltrados;
+  }
 }
